e87fcd3a2b63f841aadffd29e737a4020c93c3be
D99ZY <dtriegaardt@outlook.com>
2021-12-01 13:27:14 +0000

    Create Balance component

---
+++ expense-tracker-react/src/components/Balance.tsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+import './Balance.css';
+
+const Balance = () => {
+    return (
+        <div className='balance'>
+            <h2 className='balance--title'>Your Balance</h2>
+            <h1 className='balance--amount'>£0.00</h1>
+        </div>
+    );
+};
+
+export default Balance;
