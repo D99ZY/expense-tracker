60431a75e62927119693423f7e1b45d2cfa4f6a1
D99ZY <dtriegaardt@outlook.com>
2021-12-07 11:56:13 +0000

    Add GlobalProvider and child components

--- expense-tracker-react/src/App.tsx
+++ expense-tracker-react/src/App.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+// import React from 'react';
 import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
+
+import { GlobalProvider } from './context/GlobalState';
+
 import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <Header title='Expense Tracker' />
-        </div>  
+        <GlobalProvider>
+            <div className="app">
+                <div className="app--container">
+                    <div className="app--header-container">
+                        <Header title='Expense Tracker' />
+                    </div>
+                    <div className="app--balance-container">
+                        <Balance />
+                    </div>
+                    <div className="app--inc-exp-container">
+                        <IncomeExpenses />
+                    </div>
+                    <div className="app--transaction-list-container">
+                        <TransactionList />
+                    </div>
+                    <div className="app--add-transaction-container">
+                        <AddTransaction />
+                    </div>
+                </div>
+            </div>
+        </GlobalProvider>
     );
 }
 
