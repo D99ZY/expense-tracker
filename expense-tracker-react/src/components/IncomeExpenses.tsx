0bdd9a04f01826d958b196e4d652a1f917e731d9
D99ZY <dtriegaardt@outlook.com>
2021-12-07 11:52:04 +0000

    Add functionality to calculate income and expenses

--- expense-tracker-react/src/components/IncomeExpenses.tsx
+++ expense-tracker-react/src/components/IncomeExpenses.tsx
@@ -1,16 +1,32 @@
-// import React from 'react';
+import { useContext } from 'react';
+
+import { GlobalContext } from '../context/GlobalState';
+
 import './IncomeExpenses.css';
 
 const IncomeExpenses = () => {
+
+    const { transactions } = useContext(GlobalContext);
+
+    const amounts: number[] = transactions.map(transaction => transaction.amount);
+
+    const income: number = amounts
+        .filter(item => item > 0)
+        .reduce((a, b) => (a + b));
+
+    const expense: number = amounts
+        .filter(item => item < 0)
+        .reduce((a, b) => (a + b));
+
     return (
         <div className='inc-exp'>
             <div className='inc-exp--container'>
                 <h4>Income</h4>
-                <p id='inc-exp--amount--pos' className='inc-exp--amount pos'>+£0.00</p>
+                <p className='inc-exp--amount pos'>+£{income.toFixed(2)}</p>
             </div>
             <div className='inc-exp--container'>
                 <h4>Expenses</h4>
-                <p id='inc-exp--amount--neg' className='inc-exp--amount neg'>-£0.00</p>
+                <p className='inc-exp--amount neg'>-£{Math.abs(expense).toFixed(2)}</p>
             </div>
         </div>
     );
