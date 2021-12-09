e64a1e136f04cfd26db0b2cbca92aae06f4ef8fa
D99ZY <dtriegaardt@outlook.com>
2021-12-09 19:53:09 +0000

    Add context and fix total

--- expense-tracker-react/src/components/Balance.tsx
+++ expense-tracker-react/src/components/Balance.tsx
@@ -1,14 +1,18 @@
 import { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
-
 import './Balance.css';
 
 const Balance = () => {
+    // Context
+    const {
+        state: { transactions }
+    } = useContext(GlobalContext);
 
-    const { transactions } = useContext(GlobalContext);
-
+    // Get transaction amounts
     const amounts: number[] = transactions.map(transaction => transaction.amount);
-    const total: number = Math.round(((amounts.reduce((a, b) => a + b)) + Number.EPSILON) * 100) / 100;
+
+    // Calculate total balance
+    const total: number = Math.round((amounts.reduce((a, b) => a + b, 0) + Number.EPSILON) * 100) / 100;
 
     return (
         <div className='balance'>
