e22b87eb45c5857db0fa75f75a9624d9678d1154
D99ZY <dtriegaardt@outlook.com>
2021-12-09 19:41:45 +0000

    Add context reducer

---
+++ expense-tracker-react/src/context/AppReducer.ts
@@ -0,0 +1,21 @@
+import { GlobalState, Action } from './types';
+
+const AppReducer = (state: GlobalState, action: Action): GlobalState => {
+    switch(action.type) {
+        case 'DELETE_TRANSACTION':
+            return {
+                ...state,
+                transactions: state.transactions
+                    .filter(transaction => transaction.id !== action.payload),
+            };
+        case 'ADD_TRANSACTION':
+            return {
+                ...state,
+                transactions: [action.payload, ...state.transactions],
+            };
+        default:
+            return state;
+    }
+};
+
+export default AppReducer;
