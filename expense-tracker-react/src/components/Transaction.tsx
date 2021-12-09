7b71fa8392d7c591a583f8d32a48e531100a3aff
D99ZY <dtriegaardt@outlook.com>
2021-12-09 20:01:34 +0000

    Add context and delete transaction feature

--- expense-tracker-react/src/components/Transaction.tsx
+++ expense-tracker-react/src/components/Transaction.tsx
@@ -1,18 +1,30 @@
+import { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import { deleteTransaction } from '../context/actions';
 import './Transaction.css';
 
 interface TransactionProps {
+    id: number;
     text: string;
     amount: number;
 }
 
-const Transaction: React.FC<TransactionProps> = ({ text, amount }) => {
+const Transaction: React.FC<TransactionProps> = ({ id, text, amount }) => {
+    // Context
+    const { dispatch } = useContext(GlobalContext);
 
+    // Delete transaction from global state
+    const deleteHandler = (id: number) => dispatch(deleteTransaction(id));
+
+    // Determine amount sign
     const sign = amount < 0 ? '-' : '+';
 
     return (
-            <li className={amount < 0 ? 'transaction--row neg' : 'transaction--row pos'}>
-                <span>{text}</span><span>{sign} £{Math.abs(amount)}</span><button className='transaction--delete-btn'>X</button>
-            </li>
+        <li className={amount < 0 ? 'transaction--row neg' : 'transaction--row pos'}>
+            <span>{text}</span>
+            <span>{sign} £{Math.abs(amount)}</span>
+            <button className='transaction--delete-btn' onClick={() => deleteHandler(id)}>X</button>
+        </li>
     );
 };
 
