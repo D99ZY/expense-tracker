a35e1bf44c8d93ee30b3a1d25761631f67d07903
D99ZY <dtriegaardt@outlook.com>
2021-12-09 20:00:41 +0000

    Add context and message when no transactions

--- expense-tracker-react/src/components/TransactionList.tsx
+++ expense-tracker-react/src/components/TransactionList.tsx
@@ -1,21 +1,30 @@
 import { useContext } from 'react';
 import Transaction from './Transaction';
-
 import { GlobalContext } from '../context/GlobalState';
-
 import './TransactionList.css';
 
 const TransactionList = () => {
-
-    const { transactions } = useContext(GlobalContext);
+    // Context
+    const { 
+        state: { transactions} 
+    } = useContext(GlobalContext);
 
     return (
         <div className='transaction-list'>
             <h3 className='transaction-list--title'>History</h3>
             <ul className='transaction-list--list'>
-                {transactions.map(transaction => (
-                    <Transaction key={transaction.id} text={transaction.text} amount={transaction.amount} />
-                ))}
+                {transactions.length > 0 ? (
+                    transactions.map(transaction => (
+                        <Transaction 
+                            key={transaction.id} 
+                            id={transaction.id} 
+                            text={transaction.text} 
+                            amount={transaction.amount} 
+                        />
+                    ))
+                ) : (
+                    <span>No Transactions to Display</span>
+                )}
             </ul>
         </div>
     );
