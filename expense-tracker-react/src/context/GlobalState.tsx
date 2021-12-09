7fdf38060503bb2759741d02a59db1458c04797f
D99ZY <dtriegaardt@outlook.com>
2021-12-09 19:42:41 +0000

    Create context and provider component

---
+++ expense-tracker-react/src/context/GlobalState.tsx
@@ -0,0 +1,29 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from './AppReducer';
+import { GlobalState, Context } from './types'
+
+// Initial state
+const initialState: GlobalState = {
+    transactions: [
+        { id: 1, text: 'Salary', amount: 500 },
+        { id: 2, text: 'Book', amount: -20 },
+        { id: 3, text: 'Shoes', amount: -50 },
+    ],
+};
+
+// Create context
+export const GlobalContext = createContext<Context>({
+    state: initialState,
+    dispatch: () => undefined,
+});
+
+// Provider component
+export const GlobalProvider: React.FC = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    return (
+        <GlobalContext.Provider value={{ state, dispatch }}>
+            {children}
+        </GlobalContext.Provider>
+    );
+};
